f3ba038cd2c4d40a8edeb938632888042b4c8500
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-29 17:20:47 -0400

    intital commit

---
+++ Lab8_apapke-2.py
@@ -0,0 +1,13 @@
+"""
+Lab8_apapke-2
+Ari Papke
+geometry calcuator
+03/29/26
+"""
+
+#have to use aliases cause both modules have functions named area.
+from circle.py import area as ca
+from circle.py import circumference as cc
+from rectangle.py import area as ra
+from circle.py import perimeter as rp
+
