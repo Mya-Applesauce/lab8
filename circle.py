5878351799726e0a749bae9f751a0d4e77c7cf51
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-29 16:43:39 -0400

    added circumference

--- circle.py
+++ circle.py
@@ -1,8 +1,20 @@
+"""
+circle
+Ari Papke
+circle math
+03/23/26
+"""
+
 import math
 
 def area(radius):
     area = math.pi * (radius ** 2)
-    print(f"The area of thy circle is {area:.2f}")
+    print(f"The area of the pie is {area:.2f}")
 
-area(float(input("Enter the radius of thy circle!")))
+def circumference(radius):
+    circumference = 2 * math.pi * radius * 2
+    print(f"The circumference of the pie is {circumference:.2f}")
 
+
+area(float(input("Enter the radius of the pie!")))
+circumference(float(input("Enter the radius of the pie!")))
