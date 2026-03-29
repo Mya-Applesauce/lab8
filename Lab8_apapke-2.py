2b2c6a9fad0e173d44b86cf0a46d7f1984381e38
Mya Applesauce <myaapplesauce42@gmail.com>
2026-03-29 18:23:02 -0400

    set up the basic while loop

--- Lab8_apapke-2.py
+++ Lab8_apapke-2.py
@@ -11,4 +11,16 @@ from circle import circumference as cc
 from rectangle import area as ra
 from rectangle import perimeter as rp
 
-ca(float(input()))
+active = True
+while active:
+    choice = input((f"Welcome to the geometry calcuator!"))
+    if choice == "1":
+        ca(float(input()))
+    elif choice == "2":
+        cc(float(input()))
+    elif choice == "3":
+        ra(float(input()), float(input()))
+    elif choice == "4":
+        rp(float(input()), float(input()))
+    elif choice == "5":
+        active = False
